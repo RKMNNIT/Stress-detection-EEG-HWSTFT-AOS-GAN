ccc58f6561561c598136167ed02895c74655632f
Rakesh Rai <rakesh.mnnita@gmail.com>
2025-09-26 10:10:19 +0530

    Delete code directory

--- code/Feature_Selection.py
+++
@@ -1,5 +0,0 @@
-import scipy.io
-def RLASSO():
-    mat = scipy.io.loadmat('Segmented_EEG_Features.mat')
-    segments = mat['segmented_eeg_features']
-    print(segments)
--- code/README.md
+++
@@ -1,2 +0,0 @@
-# Code
-This folder contains source code and notebooks for the stress detection framework.
